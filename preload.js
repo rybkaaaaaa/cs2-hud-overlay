e1f61b0c5da4a0c41a4bb070a5676a633e3db302
rybkaaaaaa <playmultiganes@gmail.com>
2026-05-13 16:37:09 +0300

    Update preload.js: Add demo parser API handlers

--- preload.js
+++ preload.js
@@ -5,5 +5,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveConfig: (config) => ipcRenderer.invoke('save-config', config),
   getGameState: () => ipcRenderer.invoke('get-game-state'),
   onConfigUpdated: (callback) => ipcRenderer.on('config-updated', (event, config) => callback(config)),
-  onGameStateUpdated: (callback) => ipcRenderer.on('game-state-updated', (event, gameState) => callback(gameState))
+  onGameStateUpdated: (callback) => ipcRenderer.on('game-state-updated', (event, gameState) => callback(gameState)),
+  
+  // Demo parser APIs
+  selectDemoFile: () => ipcRenderer.invoke('select-demo-file'),
+  parseDemo: (demoPath) => ipcRenderer.invoke('parse-demo', demoPath),
+  stopDemo: () => ipcRenderer.invoke('stop-demo')
 });
