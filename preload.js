16458deecebf28a3dc671d36794307b3cf6671d1
rybkaaaaaa <playmultiganes@gmail.com>
2026-05-13 16:05:25 +0300

    Update preload.js: Add game state IPC handlers

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getConfig: () => ipcRenderer.invoke('get-config'),
   saveConfig: (config) => ipcRenderer.invoke('save-config', config),
-  updatePlayerData: (playerData) => ipcRenderer.invoke('update-player-data', playerData),
+  getGameState: () => ipcRenderer.invoke('get-game-state'),
   onConfigUpdated: (callback) => ipcRenderer.on('config-updated', (event, config) => callback(config)),
-  onPlayerDataUpdated: (callback) => ipcRenderer.on('player-data-updated', (event, playerData) => callback(playerData))
+  onGameStateUpdated: (callback) => ipcRenderer.on('game-state-updated', (event, gameState) => callback(gameState))
 });
